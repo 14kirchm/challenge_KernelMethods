4729863cec9bb98c4e62ed690780df589e175595
Thomas Debarre <thomas.debarre@gmail.com>
2017-03-10 15:11:42 +0100

    useless import deleted

--- lib/svm.py
+++ lib/svm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import linalg
 import cvxopt  # https://courses.csail.mit.edu/6.867/wiki/images/a/a7/Qp-cvxopt.pdf
-import pylab as plt
+import pylab as pl
 import matplotlib.pyplot as plt
 import os.path
 from numpy import save, load
